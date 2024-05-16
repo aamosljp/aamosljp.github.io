5d7de3c33b6646b4fd95dceedab3eda2360b6780
Aamos Pernu <aamos.pernu@outlook.com>
2024-05-17 02:36:53 +0300

    Trying stuff

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => (
   <div className="container mx-auto p-4">
     <h2 className="text-2xl font-bold mb-4">About</h2>
-    <p>About content goes here.</p>
+    <p>HELLOOOOOO!!!!</p>
   </div>
 );
 
