b40933f71d47b72f5769f35b5a4d1cfdcbd194cd
Aamos Pernu <aamos.pernu@outlook.com>
2024-05-18 04:10:02 +0300

    Visual feedback when submitting

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,14 +7,18 @@ const Contact = () => {
         email: '',
         message: '',
     });
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [error, setError] = useState(null);
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
     };
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setIsSubmitting(true);
+        setError(null);
         const payload = { data: formData };
-        console.log('Payload:', payload);
         const request = {
             method: 'POST',
             headers: {
@@ -22,39 +26,47 @@ const Contact = () => {
             },
             body: JSON.stringify(payload),
         };
-        console.log('Request:', request);
         try {
             const response = await fetch('https://passionate-fruit-347d6d7acb.strapiapp.com/api/messages', request);
-            console.log('Response:', response);
             if (response.ok) {
-                console.log('Email sent');
+                setIsSubmitted(true);
             } else {
-                const errorData = await response.json();
-                console.error('Error response:', errorData);
+                setError('Failed to send message.');
             }
         } catch (error) {
-            console.error(error);
+            setError('An error occurred while sending the message.');
+        } finally {
+            setIsSubmitting(false);
         }
     };
     return (
         <div className="container mx-auto p-4">
             <h2 className="dark:text-blue-300 mb-4 transition duration-300">Contact</h2>
-            <form onSubmit={handleSubmit} className="space-y-4">
-                <div>
-                    <label className="dark:text-blue-200 block text-gray-700 transition duration-300">Name:</label>
-                    <input type="text" name="name" value={formData.name} onChange={handleChange} required className="rounded dark:border-gray-700 dark:bg-gray-600 w-full border border-gray-300 p-2 transition duration-300" />
-                </div>
-                <div>
-                    <label className="dark:text-blue-200 block text-gray-700 transition duration-300">Email:</label>
-                    <input type="email" name="email" value={formData.email} onChange={handleChange} required className="rounded dark:border-gray-700 dark:bg-gray-600 w-full border border-gray-300 p-2 transition duration-300" />
-                </div>
-                <div>
-                    <label className="dark:text-blue-200 block text-gray-700 transition duration-300">Message:</label>
-                    <textarea name="message" value={formData.message} onChange={handleChange} className="rounded dark:border-gray-700 dark:bg-gray-600 w-full border border-gray-300 p-2 transition duration-300" />
-                </div>
-                <button type="submit" className="rounded dark:bg-blue-800 bg-blue-500 text-white px-4 py-2 transition duration-300">Submit</button>
-            </form>
-        </div >
+            {isSubmitted ? (<div className="flex flex-col space-y-4">
+                <h2>Thank you!</h2>
+                <p>Your message has been sent successfully.</p>
+            </div>
+            ) : (
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div>
+                        <label className="dark:text-blue-200 block text-gray-700 transition duration-300">Name:</label>
+                        <input type="text" name="name" value={formData.name} onChange={handleChange} required className="rounded dark:border-gray-700 dark:bg-gray-600 w-full border border-gray-300 p-2 transition duration-300" />
+                    </div>
+                    <div>
+                        <label className="dark:text-blue-200 block text-gray-700 transition duration-300">Email:</label>
+                        <input type="email" name="email" value={formData.email} onChange={handleChange} required className="rounded dark:border-gray-700 dark:bg-gray-600 w-full border border-gray-300 p-2 transition duration-300" />
+                    </div>
+                    <div>
+                        <label className="dark:text-blue-200 block text-gray-700 transition duration-300">Message:</label>
+                        <textarea name="message" value={formData.message} onChange={handleChange} className="rounded dark:border-gray-700 dark:bg-gray-600 w-full border border-gray-300 p-2 transition duration-300" />
+                    </div>
+                    <button type="submit" disabled={isSubmitting} className="rounded dark:bg-blue-800 bg-blue-500 text-white px-4 py-2 transition duration-300">
+                        {isSubmitting ? 'Submitting...' : 'Submit'}
+                    </button>
+                    {error && <p style={{ color: 'red' }}>{error}</p>}
+                </form>
+            )}
+        </div>
     )
 };
 
